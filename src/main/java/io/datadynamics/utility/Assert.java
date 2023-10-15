a454cac9b7f89d3f6aa5b4484a757173640d644c
김병곤 <fharenheit@gmail.com>
2023-10-15 20:35:31 +0900

    유틸리티 추가

--- src/main/java/io/datadynamics/utility/Assert.java
+++ src/main/java/io/datadynamics/utility/Assert.java
@@ -748,4 +748,14 @@ public abstract class Assert {
         return (messageSupplier != null ? messageSupplier.get() : null);
     }
 
+    /**
+     * Checks that the given argument is not null. If it is, throws {@link NullPointerException}.
+     * Otherwise, returns the argument.
+     */
+    public static <T> T checkNotNull(T arg, String text) {
+        if (arg == null) {
+            throw new NullPointerException(text);
+        }
+        return arg;
+    }
 }
