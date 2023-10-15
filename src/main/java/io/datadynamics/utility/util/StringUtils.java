15663504bd5350a2141547c6611df26382ffdcce
김병곤 <fharenheit@gmail.com>
2023-10-15 11:13:53 +0900

    라이브러리 추가

--- src/main/java/io/datadynamics/utility/util/StringUtils.java
+++ src/main/java/io/datadynamics/utility/util/StringUtils.java
@@ -2,12 +2,6 @@ package io.datadynamics.utility.util;
 
 public class StringUtils {
 
-    /**
-     * Check if the String is null or has only whitespaces.
-     *
-     * @param string String to check
-     * @return {@code true} if the String is null or has only whitespaces
-     */
     public static boolean isBlank(String string) {
         if (isEmpty(string)) {
             return true;
@@ -20,44 +14,18 @@ public class StringUtils {
         return true;
     }
 
-    /**
-     * Check if the String has any non-whitespace character.
-     *
-     * @param string String to check
-     * @return {@code true} if the String has any non-whitespace character
-     */
     public static boolean isNotBlank(String string) {
         return !isBlank(string);
     }
 
-    /**
-     * Check if the String is null or empty.
-     *
-     * @param string String to check
-     * @return {@code true} if the String is null or empty
-     */
     public static boolean isEmpty(String string) {
         return string == null || string.isEmpty();
     }
 
-    /**
-     * Check if the String has any character.
-     *
-     * @param string String to check
-     * @return {@code true} if the String has any character
-     * @since 1.1.0
-     */
     public static boolean isNotEmpty(String string) {
         return !isEmpty(string);
     }
 
-    /**
-     * Truncate the String to the max length.
-     *
-     * @param string    String to truncate
-     * @param maxLength max length
-     * @return truncated String
-     */
     public static String truncate(String string, int maxLength) {
         if (string.length() > maxLength) {
             return string.substring(0, maxLength);
@@ -65,14 +33,6 @@ public class StringUtils {
         return string;
     }
 
-    /**
-     * Truncate the String to the max length.
-     *
-     * @param string    String to truncate
-     * @param maxLength max length
-     * @param suffix    String Suffix
-     * @return truncated String
-     */
     public static String truncate(String string, int maxLength, String suffix) {
         return truncate(string, maxLength) + suffix;
     }
